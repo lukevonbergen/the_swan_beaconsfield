107974207e2c23f581836936d5db8102f34e1f7a
lukevonbergen <luke_vb1@outlook.com>
2025-09-10 11:54:20 +0100

    password to enter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import WelcomeOverlay from './components/WelcomeOverlay';
+import PasswordGate from './components/PasswordGate';
 import Home from './pages/Home';
 import About from './pages/About';
 import Menu from './pages/Menu';
@@ -15,19 +16,21 @@ function App() {
   const isHomePage = location.pathname === '/';
 
   return (
-    <div className="App">
-      {isHomePage && <WelcomeOverlay />}
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/book" element={<Book />} />
-      </Routes>
-      <Footer />
-    </div>
+    <PasswordGate>
+      <div className="App">
+        {isHomePage && <WelcomeOverlay />}
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/book" element={<Book />} />
+        </Routes>
+        <Footer />
+      </div>
+    </PasswordGate>
   );
 }
 
